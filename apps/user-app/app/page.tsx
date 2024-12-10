a80372970420a0be49c50c9250a524dc2c9fe5d1
Rohan Mathur <rm30092000mr@gmail.com>
2024-12-10 15:32:02 +0530

    Checking Prisma Initialization

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,4 +1,8 @@
+import { PrismaClient } from "@repo/db/client";
+const client = new PrismaClient()
+
 export default function Page() {
+  console.log(client)
   return (
     <div className="text-2xl flex justify-center items-center h-screen">
       Hi There, Rohan
