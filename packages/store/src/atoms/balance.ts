cd0c1032b5ac63eda992ae9866e4ecbcd825d7d4
Rohan Mathur <rm30092000mr@gmail.com>
2024-12-12 21:37:15 +0530

    Using Jotai

---
+++ packages/store/src/atoms/balance.ts
@@ -0,0 +1,9 @@
+// import { atom } from "recoil";
+import { atom } from "jotai";
+
+// export const balanceAtom = atom<number>({
+//     key: "balance",
+//     default: 0
+// })
+
+export const balanceAtom = atom("$1,000,000,000");
