e383538ea986e915d52e6099197ffcd40a4e7de8
Rohan Mathur <rm30092000mr@gmail.com>
2024-12-17 16:24:52 +0530

    Adding Next Auth in User App

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,12 +1,17 @@
-import { PrismaClient } from "@repo/db/client";
+"use client"
+
 import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
 import { Button } from "./components/Button";
-const client = new PrismaClient()
+import { Appbar } from "@repo/ui/appbar";
 
 export default function Page() {
-  console.log(client)
+  const session = useSession();
   return (
     <div className="flex flex-col justify-center items-center h-screen gap-2">
+      <div className="absolute top-0 w-full">
+        <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
+      </div>
       <p className="text-2xl">Hi There, Rohan</p>
       <Button>
         <Link href="balance" className="text-sm px-2 py-1">
